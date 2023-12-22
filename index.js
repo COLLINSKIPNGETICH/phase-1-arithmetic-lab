c567c2b6ac89a7b564f4724bee8012d15904e447
+collins <+collinskipngetich60@gmail.com>
2023-12-22 16:54:08 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// The variables thet multiplys to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// Generathing a random integer between 1 and 100
+let random = Math.floor(Math.random() * 100) + 1; 
+
+// Finding the remainder
+let num3 = 23;
+let num4 = 4;  // Adjusted value
+// finding result
+let mod = (num3 % num4) +1;
+ 
+
+// Displaying the highest number
+let max = Math.max(2, 6, 10, 15, 20); 
